7a89576af19983479e5182153f9c704ce1506ea9
Roma Bulani <romabulani@gmail.com>
2021-08-29 00:05:19 +0530

    Added back button

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var billAmount = document.querySelector("#input-billamount");
 var cashGiven = document.querySelector("#input-cashgiven");
 var btnNext = document.querySelector("#next-button");
 var btnCheck = document.querySelector("#check-button");
+var btnBack = document.querySelector("#back-button");
 var notesAvailable = document.querySelectorAll(".availableNotes");
 var errorTag = document.querySelector("#error-message");
 var notesArray = [2000, 500, 100, 20, 10, 5, 1];
@@ -41,15 +42,27 @@ function checkCashGiven(){
     var cashGivenValue = Number(cashGiven.value);
     var billAmountValue = Number(billAmount.value);
 
-    if(cashGivenValue < billAmountValue)
+    if(cashGivenValue <= 0)
+        showError("Cash Given should be greater than 0");
+    else if(cashGivenValue < billAmountValue)
         showError("Cash Given should be atleast equal to Bill Amount");
     else
         calculate(billAmountValue,cashGivenValue);
 }
 
+function backAction(){
+    console.log("clicked");
+    billAmount.disabled = false;
+    btnNext.style.display = "block";
+    document.querySelector("#second-container").style.display = "none";
+    document.querySelector("#third-container").style.display = "none";
+}
+
 btnNext.addEventListener("click", checkBillAmount);
 
 btnCheck.addEventListener("click", checkCashGiven);
+
+btnBack.addEventListener("click",backAction);
 
 
 
